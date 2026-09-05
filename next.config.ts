8d3d4e0443eaec02ca9319662c9ad4e72e173350
mommyy <jamesjayawara@gmail.com>
2026-09-05 09:25:13 +0700

    updated image base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+
+
+const isProduction = process.env.NODE_ENV == "production"
 const nextConfig: NextConfig = {
+  basePath: isProduction?"/Portofolio":"",  
   output: "export",
   distDir: "dist"
   /* config options here */
